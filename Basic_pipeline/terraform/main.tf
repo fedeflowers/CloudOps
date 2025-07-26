2001e76d9ee755236b4d173a866e84756d7cf799
Federico Fiorio <fioriofederico99@gmail.com>
2025-07-26 21:24:54 +0200

    backedn

--- Basic_pipeline/terraform/main.tf
+++ Basic_pipeline/terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.37.0"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
