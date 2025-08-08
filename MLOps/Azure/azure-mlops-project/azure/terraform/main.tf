6e7ac01a5bdcf54b51a6ee90c5de0eb40ebe18bf
Federico Fiorio <f.fiorio@reply.it>
2025-08-08 18:08:16 +0200

    bugfix env

--- MLOps/Azure/azure-mlops-project/azure/terraform/main.tf
+++ MLOps/Azure/azure-mlops-project/azure/terraform/main.tf
@@ -1,11 +1,13 @@
 terraform {
   required_version = ">= 1.5.5"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.114.0"
     }
   }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
