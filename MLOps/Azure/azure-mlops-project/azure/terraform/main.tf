6b7b8f26f2704a97d1f8d6d356a7973781b61b33
Federico Fiorio <f.fiorio@reply.it>
2025-08-08 17:57:17 +0200

    update vars

--- MLOps/Azure/azure-mlops-project/azure/terraform/main.tf
+++ MLOps/Azure/azure-mlops-project/azure/terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.5.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -11,10 +11,6 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-variable "prefix" { type = string }
-variable "location" { type = string }
-variable "environment" { type = string }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-${var.environment}-rg"
@@ -28,12 +24,6 @@ module "ml_core" {
   location    = var.location
   rg_name     = azurerm_resource_group.rg.name
 }
-
-output "ml_workspace_name" { value = module.ml_core.workspace_name }
-output "acr_name" { value = module.ml_core.acr_name }
-output "kv_name" { value = module.ml_core.kv_name }
-output "storage_account" { value = module.ml_core.storage_account }
-
 
 
 
